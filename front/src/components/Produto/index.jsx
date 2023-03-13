b2ea4b39120a3a0d990ec54fbb7d11e4ab8646c2
edmilsonmiranda <jessicamaiaaraujo@gmail.com>
2023-03-13 11:41:11 -0300

    stack

--- front/src/components/Produto/index.jsx
+++ front/src/components/Produto/index.jsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Paper, Stack } from "@mui/material";
 import React from "react";
 import sapato from "./sapato.png";
 
@@ -6,11 +6,13 @@ export default function Produto()
 {
     return(
         <>
+          <Stack>
           <Paper elevation={2}> 
              <img src={sapato}/>
           </Paper>
           <span>Categoria</span>
           <h4>Nome do Produto</h4>
+          </Stack>
         </>
     )
 }
