6fd2b7b8e8e2a232f77c10095d05c39818ad585d
edmilsonmiranda <jessicamaiaaraujo@gmail.com>
2023-03-15 10:29:37 -0300

    detalhes

--- front/src/pages/DetalhesProduto/index.js
+++ front/src/pages/DetalhesProduto/index.js
@@ -1,17 +1,62 @@
 import { useParams } from "react-router-dom";
-import { Grid } from "@mui/material";
+import { Card, Grid } from "@mui/material";
+import adidas from "./img/adidas.png";
+import air from "./img/air.png";
+import Sneakers from "./img/Sneakers.png";
+import nike from "./img/nike.jpg";
+import nike1 from "./img/nike1.jpg";
+
+import React from "react";
 
 export default function DetalhesProduto() {
     const {id} = useParams();
+    const [atual, setAtual] = React.useState(0);
+
+    const imagens = [
+        adidas,
+        Sneakers,
+        air,
+        nike,
+        nike1,
+    ];
+
+    const Items = () => {
+        
+
+        return imagens.map((cada, posicao) => {
+            return (
+                
+                    <Grid item xs={2.4}>
+                        <Card onClick={() => setAtual(posicao)}>
+                            <img src={cada} width="100%"/>
+                        </Card>
+                    </Grid>
+                
+            );
+        });
+    }
 
     return (
         <div>
             Detalhes do Produto {id}
 
-            <Grid container>
+            <Grid container spacing={3}>
                 <Grid item xs={7}>
-                Fotos
+                 <Card align="center">
+                    <img width="400px" src={imagens[atual]}/>
+                 </Card>
+
+
+                   <Grid container spacing={2}>
+                    
+                        <Items/>
+
+
+                    </Grid>
+                
                 </Grid>
+
+                
 
                 <Grid item xs={5}>
                 Informações
